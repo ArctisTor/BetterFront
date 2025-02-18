89d9c05f11a127eb142628a0a4f8f6cb0aad0110
ArctisTor <cplatz@citadel.edu>
2025-02-18 14:59:08 -0500

    First pass through of routes.

--- VTuber-start/VTuber-Angular/src/app/app.component.ts
+++ VTuber-start/VTuber-Angular/src/app/app.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { VtuberHeaderComponent } from './header/vtuber-header.component';
-import { VTuberListComponent } from './vtuber/vtuber-list/vtuber-list.component';
-import { VtuberService } from './services/vtuber.service';
+import { Component, OnInit } from '@angular/core'
+import { Router, RouterOutlet } from '@angular/router'
+import { VtuberHeaderComponent } from './header/vtuber-header.component'
+import { VTuberListComponent } from './vtuber/vtuber-list/vtuber-list.component'
+import { VtuberService } from './services/vtuber.service'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, VtuberHeaderComponent, VTuberListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, VtuberHeaderComponent, VTuberListComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'VTuber Database';
+    title = 'VTuber Database'
 
-  constructor(private vtuberService: VtuberService) {}
+    constructor(private vtuberService: VtuberService, private router: Router) {}
 
-  ngOnInit(): void {
-   this.vtuberService.fetchVTubers(); 
-  }
+    ngOnInit(): void {
+        this.vtuberService.fetchVTubers()
+    }
 }
--- VTuber-start/VTuber-Angular/src/app/app.routes.ts
+++ VTuber-start/VTuber-Angular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path : 'organization', 
+        loadComponent: () => import('./organizations/organization/organization.component').then(m => m.OrganizationComponent)
+    }
+];
